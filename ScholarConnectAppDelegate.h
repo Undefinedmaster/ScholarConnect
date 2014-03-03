8342b01660c886a5fcd55ef60e04d8afa54b8ec7
Undefinedmaster <ezra.kalmowitz@gmail.com>
2014-03-02 19:27:34 -0500

    Create ScholarConnectAppDelegate.h

---
+++ ScholarConnectAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SimpleBrowserViewController.m
+//  SimpleBrowser
+//
+//  Created by Ezra Kalmowitz and Alex Raistrick on 2/16/14.
+//  Copyright (c) 2013 Ezra Kalmowitz and Alex Raistrick. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleBrowserAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
