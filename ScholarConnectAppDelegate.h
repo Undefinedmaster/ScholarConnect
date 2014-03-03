4458f1d68595309de3283d44b4f4e860b809d498
Undefinedmaster <ezra.kalmowitz@gmail.com>
2014-03-02 19:28:59 -0500

    Update ScholarConnectAppDelegate.h

--- ScholarConnectAppDelegate.h
+++ ScholarConnectAppDelegate.h
@@ -1,7 +1,4 @@
 //
-//  SimpleBrowserViewController.m
-//  SimpleBrowser
-//
 //  Created by Ezra Kalmowitz and Alex Raistrick on 2/16/14.
 //  Copyright (c) 2013 Ezra Kalmowitz and Alex Raistrick. All rights reserved.
 //
